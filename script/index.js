2913e0817b5e16f9e63308946d2c1fa918df741a
Mafalda1@ <samuelpds77@gmail.com>
2026-04-17 17:15:20 -0300

    ADICIONADO PARTE LATERAL

--- script/index.js
+++ script/index.js
@@ -1,10 +1,19 @@
 let CampoEditarPai = document.getElementById("CampoEditarPai")
 let CampoEditarFilho = document.getElementById("CampoEditarFilho")
 
+//MOSTRAR O CAMPO PARA EDITAR PRODUTOS
 function mostrar(){
     
 
     CampoEditarFilho.classList.toggle("-translate-y-full")
     CampoEditarPai.classList.toggle("opacity-0")
     CampoEditarPai.classList.toggle("pointer-events-none")
+}
+
+
+//MOSTRAR O CAMPO PARA NAVEGAR ENTRE AS PAGINAS
+let lateral = document.getElementById("lateral")
+
+function mostrarOpçoes(){
+    lateral.classList.toggle("hidden")
 }
